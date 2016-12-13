f4077195c91329245511b1132fbdecf637aac445
sejanH <sejan840@gmail.com>
2016-12-13 17:48:45 +0600

    13/12/2016

--- auth/logout.php
+++ auth/logout.php
@@ -3,17 +3,17 @@ session_start();
 
 if(!isset($_SESSION['user']))
 {session_destroy();
- header("Location: /quote/index.php");
+ header("Location:/index.php");
 }
 else if(isset($_SESSION['user'])!="")
 {session_destroy();
- header("Location: /quote/index.php");
+ header("Location:/index.php");
 }
 
 if(isset($_GET['logout']))
 {
  session_destroy();
  unset($_SESSION['user']);
- header("Location: /quote/index.php");
+ header("Location:/index.php");
 }
 ?>
